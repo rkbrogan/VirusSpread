03eb228a2fbc4dc3fc619abcfeea4583669b05a2
Riley Brogan <riley.k.brogan@gmail.com>
2023-12-30 22:03:58 -0800

    Nearly finished out simulate.c. Warnings?

--- src/simulate.c
+++ src/simulate.c
@@ -13,6 +13,7 @@
 #include "virus.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
 
@@ -109,6 +110,62 @@ int _main (int argc, char* argv[])
     FILE *output;
 
     // Insert the rest of the program here
+    
+    // Check number of command line arguments
+    if (argc != 3)
+    {
+        printf("Not enough command line arguments.");
+        return -1;
+    }
+
+    // Set values from program arguments
+    vprob = atof(argv[1]);
+    tprob = atof(argv[2]);
+    // TODO: Error check vprob and tprob
+
+    output =  fopen(argv[3], "w");
+    // TODO: Error check output
+
+    // Calculate number of vaccinated people
+    int nvaccinated  = (int)(vprob * npeople);
+
+    // Set initial status of each person
+    initialize_status(npeople, initial_infections, nvaccinated, status);
+
+    // Simulate 100 days
+    for (int day = 0; day < 100; day++)
+    {
+        // Simulate through population
+        for (int person = 0; person < npeople; person++)
+        {
+            // Get status of person
+            int personStatus = status[person];
+
+            // Update status of person for next day
+            // If person is susceptible, check if they get virus
+            // Else if person is sick, see if they die
+            // Otherwise person stays same status
+            if (personStatus == is_susceptible)
+            {
+                newstatus[person] = catch_or_not(tprob, npeople, status);
+            }
+            else if (personStatus >= is_infected)
+            {
+                newstatus[person] = die_or_not(dprob, sick_days, person, npeople, status);
+            }
+            else
+            {
+                newstatus[person] = personStatus;
+            }
+        }
+
+        // Copy newstatus to status
+        update_status(npeople, status, newstatus);
+
+        // Print daily stats
+        printf("\n");
+        fprintf(output, "%d %d %d %d %d %d\n", day, nsusceptible, nrecovered, nvaccinated, ninfected, ndead);
+    }
 
     // printf ( "Population: %d\n", npeople );
     // printf ( "Vaccination Probability: %lf\n", vprob );
@@ -118,4 +175,6 @@ int _main (int argc, char* argv[])
     // printf ( "Number of Recovered: %d\n", nrecovered );
     // printf ( "Number of Dead: %d\n", ndead );
     // printf ( "Case Fatality Rate: %lf\n", (double)ndead/(nrecovered+ndead) );
+
+    return 0;
 }
